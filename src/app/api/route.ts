4db4b42eb847d40412a96d8444072760d0b3cb97
Magnus Bull <magnus@norkred.no>
2025-09-01 09:01:04 +0200

    ddg

---
+++ src/app/api/route.ts
@@ -0,0 +1,17 @@
+import { imageSearch } from "@mudbill/duckduckgo-images-api";
+
+export async function GET() {
+  console.log("Searching DDG for images...");
+
+  const images = await imageSearch({
+    query: "pikachu",
+  });
+
+  console.log("Got", images.length, "results");
+
+  return new Response(JSON.stringify(images), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
